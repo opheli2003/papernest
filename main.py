507ac3326fbe661d9d59ff05eb772ca11fc871b4
opheli2003 <90799762+opheli2003@users.noreply.github.com>
2025-01-20 13:09:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -69,5 +69,5 @@ async def get_coverage_endpoint_response(request: AddressRequest) -> Dict[str, D
     return await get_coverage_datas(request)
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
